ec53feaabc1782645bc108b5aa7113c006c26615
sandraortiz <rocio12vx@gmail.com>
2018-07-11 11:26:50 -0500

    practicando firebase

---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+const config = {
+    apiKey: "AIzaSyD0yMj1HAVdrbRGELeMSa5CldOx3-epQ1Q",
+    authDomain: "socialnetworktrinitycoders.firebaseapp.com",
+    databaseURL: "https://socialnetworktrinitycoders.firebaseio.com",
+    projectId: "socialnetworktrinitycoders",
+    storageBucket: "",
+    messagingSenderId: "204933739842"
+  };
+  firebase.initializeApp(config);
+
+
+
---
+++ src/main.js
@@ -0,0 +1,11 @@
+const buttonSingup=document.getElementById('singup');
+const userEmail =document.getElementById('email');
+const userPassword =document.getElementById('passsword');
+
+buttonSingup.addEventListener('click'  , () => {
+ const userEmailv = userEmail.value;
+ const userPasswordv = userPassword.value;
+ 
+    });
+
+  
