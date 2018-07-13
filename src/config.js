0fd3392b65150c522a5fe4b0dda1f85fb97ab2a6
Lizethsuk <julia.sucasacai@gmail.com>
2018-07-13 16:33:23 -0500

    CSS inicio

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Initialize Firebase
 var config = {
-    //apiKey: "AIzaSyD33a72Urx8vLxoSDdJu0woInLXFQ3yq3M",
+    apiKey: "AIzaSyD33a72Urx8vLxoSDdJu0woInLXFQ3yq3M",
     authDomain: "socialnetwork-165d4.firebaseapp.com",
     databaseURL: "https://socialnetwork-165d4.firebaseio.com",
     projectId: "socialnetwork-165d4",
